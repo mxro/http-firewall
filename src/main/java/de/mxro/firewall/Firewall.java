9fab1bc10154eaa8b57dc278b7dcc1ba02bfe258
mxro <mxro@nowhere.com>
2014-08-05 11:19:21 +1000

    Improving http-server API / using service-api as base for HttpService Autocommit Tue Aug 05 11:19:21 EST 2014

--- src/main/java/de/mxro/firewall/Firewall.java
+++ src/main/java/de/mxro/firewall/Firewall.java
@@ -3,22 +3,22 @@ package de.mxro.firewall;
 import java.util.List;
 
 import de.mxro.firewall.internal.ProtectedService;
-import de.mxro.httpserver.StoppableService;
+import de.mxro.httpserver.HttpService;
 import de.mxro.httpserver.services.Services;
 
 public class Firewall {
 
-	public static final StoppableService protect(StoppableService service, Rule rule) {
+	public static final HttpService protect(HttpService service, Rule rule) {
 		
 		return Services.safeShutdown(new ProtectedService(service, rule));
 		
 	}
 	
-	public static final StoppableService protect(StoppableService service, List<Rule> rules) {
+	public static final HttpService protect(HttpService service, List<Rule> rules) {
 		return protect(service, rules, 0);
 	}
 	
-	private static final StoppableService protect(StoppableService service, List<Rule> rules, int fromIdx) {
+	private static final HttpService protect(HttpService service, List<Rule> rules, int fromIdx) {
 		if (fromIdx >= rules.size()) {
 			return service;
 		}
