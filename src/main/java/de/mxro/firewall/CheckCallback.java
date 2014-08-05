5bc0587b3b6d93402ae1fb5a4113221b8c8f45e7
mxro <mxro@nowhere.com>
2014-08-05 11:21:21 +1000

    Improving http-server API / using service-api as base for HttpService Autocommit Tue Aug 05 11:21:21 EST 2014

--- src/main/java/de/mxro/firewall/CheckCallback.java
+++ src/main/java/de/mxro/firewall/CheckCallback.java
@@ -6,18 +6,26 @@ public interface CheckCallback {
 	 * Call if the request is allowed.
 	 */
 	public void allow();
-	
+
 	/**
 	 * Call if the request is not allowed. Response object should be populated.
 	 */
 	public void block();
-	
+
 	/**
-	 * Call if the request is not allowed. Response object will be populated with provided message.
+	 * Call if the request is not allowed. Response object will be populated
+	 * with provided message.
+	 * 
 	 * @param cause
 	 */
 	public void block(String cause);
-	
+
+	/**
+	 * Call if an unexpected failure occurs. Response object will be populated
+	 * with provided exception.
+	 * 
+	 * @param t
+	 */
 	public void onFailure(Throwable t);
-	
+
 }
